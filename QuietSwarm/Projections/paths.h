3e62eef5a6eb309f774c0eea53415e4c6b833426
FreddiiPl <106398563+FreddiiPl@users.noreply.github.com>
2026-05-11 22:15:24 +0200

    Delete QuietSwarm/Projections/paths.h

--- QuietSwarm/Projections/paths.h
+++
@@ -1,9 +0,0 @@
-#ifndef PATHS_H
-#define PATHS_H
-
-#include <stddef.h>
-
-int get_executable_directory(char *output_path, size_t max_size);
-void getAbsolutePath(char *output_buffer, size_t max_size, const char *relpath);
-
-#endif
