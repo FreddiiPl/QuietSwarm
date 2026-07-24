1ec359c59651acc90f3ca9360575135d0e50bc50
FreddiiPl <106398563+FreddiiPl@users.noreply.github.com>
2026-07-24 12:50:58 +0200

    Refactor Swarm class initialization and cleanup
    Refactor Swarm class to accept orbital parameters directly and remove unused matplotlib imports.

--- QuietSwarm/Classes/Swarm.py
+++ QuietSwarm/Classes/Swarm.py
@@ -3,92 +3,27 @@ from .objectTypes import objectTypes
 from QuietSwarm.Helpers.Projections import eciToecef, ecefTolla, llaToEcef
 from QuietSwarm.Helpers.wgs84 import EARTH_SEMI_MAJOR_AXIS, EARTH_MU
 
-import matplotlib.pyplot as plt
-import matplotlib as mpl
-
-# Default figure settings
-mpl.rcParams['axes.linewidth'] = 1.2
-mpl.rcParams['xtick.direction'] = "in"
-mpl.rcParams['xtick.top'] = True
-mpl.rcParams['ytick.direction'] = "in"
-mpl.rcParams['ytick.right'] = True
-mpl.rcParams['xtick.minor.visible'] = True
-mpl.rcParams['ytick.minor.visible'] = True
-mpl.rcParams['font.weight'] = "bold"
-mpl.rcParams['axes.labelweight'] = "bold"
-mpl.rcParams['font.size'] = 12
-
-
 class Swarm:
     '''
-    Main swarm class. Handles the state variables of swarm given a set of
-    initial orbital properties:
-    
-    - right ascension of ascending node
-    - argument of periapsis/perigee
-    - inclination angle
-    - phase in orbital plane
-    - semi-major axis
-    - eccentricity
+    Params -> orbital parameters as input argument.
+        Should contain 6 arrays - each of size nr_sats:
+            - raan
+            - argp
+            - incl_rad
+            - phases_rad
+            - sma
+            - ecc
     '''
     
-    def __init__(self,orbitalFile):
+    def __init__(self,
+                 params):
         self.types = objectTypes()
+        
+        self.nr_sats     = params.shape[1]
+        self.orbitParams = params
+   
 
-        # To be cleaned up!!
-        with open(orbitalFile, "r") as f:
-            nr_configurations = sum(1 for line in f) - 1
-            self.nr_sats = 0
-            self.temp = np.zeros((nr_configurations, 6)) # ghost number 6...
-            
-            
-            f.seek(0)
-            next(f)
-            for i, row in enumerate(f):
-                
-                values = [float(x) for x in row.strip().split(",")]
-                n_sat     = values[0]
-                apoapsis  = values[1]
-                periapsis = values[2]
-                
-                semiMajorAxis = (apoapsis + periapsis) / 2
-                semiMajorAxis += EARTH_SEMI_MAJOR_AXIS / 1e3
-                eccentricity  = (apoapsis - periapsis) / (apoapsis + periapsis)
-  
-                values[1] = semiMajorAxis
-                values[2] = eccentricity
-                
-                self.nr_sats += int(n_sat)
-                self.temp[i, :] = values
-            
-            
-            self.orbitParams = np.zeros((self.nr_sats, 6))
-            sat_idx = 0
-            for config in self.temp:
-                n_sat = int(config[0])
-                phases = np.linspace(0, 360, n_sat, endpoint=False)
-                
-                raan_rad = np.deg2rad(config[3])
-                argp_rad = np.deg2rad(config[4])
-                incl_rad = np.deg2rad(config[5])
-                phases_rad = np.deg2rad(phases)
-                for k in range(n_sat):
-                    params = [
-                            raan_rad,  # raan
-                            argp_rad,  # argp
-                            incl_rad,   # inc
-                            phases_rad[k],  # phase
-                            config[1] * 1e3,  # sma
-                            config[2],  # ecc
-                        ]
-                    
-                    self.orbitParams[sat_idx, :] = params
-                    sat_idx += 1
-            
-
-  
-    
-    def propagate(self, tmax, dt):
+    def propagate(self, tmax, dt, utc=np.datetime64(datetime.now(), "ms")):
         c_propagator = self.types.propagator_c()
         OrbitArrayType = self.types.orbit_param * len(self.orbitParams)
         
@@ -105,9 +40,7 @@ class Swarm:
         stride     = int(1.0 // dt) 
         if stride < 1: stride = 1
         
-        
         output_ptr = c_propagator.propagate(n_steps, h_norm, self.nr_sats, orbit_array, stride)
-        
         
         # Transfer output
         dtype = np.dtype([
@@ -127,7 +60,6 @@ class Swarm:
         output['x'] *= a_normalizer
         output['y'] *= a_normalizer
         output['z'] *= a_normalizer
-
         c_propagator.free_output(output_ptr)
         
         print("\n--- PYTHON SIDE DEBUG ---")
@@ -143,6 +75,7 @@ class Swarm:
         Based on IERS conventions -> GCRS - ITRF conversion using implemented precession-nutation model (IAU2000/2006)
         '''
         
+        # for better accuracy we need to implement correction between ut1 and utc
         refsystem      = self.types.julianDate()
         UT1_encoded    = UT1_time.encode('utf-8')
         
@@ -213,13 +146,26 @@ class Swarm:
         
         return result
 
+
     def obstruction(self, states, observer: tuple, elevation_map: np.ndarray):
         # calculate path of satellite through elevation map and determine if it is obstructed
-        # almost ray tracing.
+        # ray marching
         
         
-        pass
+        raise NotImplementedError("Not implemented yet")
 
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
         
         
         
